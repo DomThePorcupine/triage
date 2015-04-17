a482be455090133d2fa9049855bfd17ab5ea4d82
DomThePorcupine <porcupinebot@gmail.com>
2015-04-17 07:34:55 -0400

    extensive work now launcher acts as updater

--- launcher.py
+++ launcher.py
@@ -12,17 +12,39 @@ class mainGUI(Tkinter.Tk):
 		self.initialize()
 		
 	def initialize(self):
-		button = Tkinter.Button(self,text="Click to begin!",command=self.OnButtonClick)
+		button = Tkinter.Button(self,text="Click here to launch GUI (Recommended)",command=self.OnButtonClick)
+		buttonCLI = Tkinter.Button(self,text="Click here for CLI (Admin/Kevin/Dom)",command=self.OnCLIButtonClick)
 		button.grid(column=0,row=0)
+		buttonCLI.grid(column=0,row=1)
 	def OnButtonClick(self):
-		os.system("xterm -e \"sudo python2 guifrontend.py\"")
+		
+		os.system("xterm -e \"sudo python2 /usr/local/bin/guifrontend.py\"")
+		self.destroy()
+	def OnCLIButtonClick(self):
+		os.system("xterm -e \"sudo python2 /usr/local/bin/triage.py\"")
+		self.destroy()
+class updater(Tkinter.Tk):
+	def __init__(self, parent):
+		Tkinter.Tk.__init__(self, parent)
+		self.parent=parent
+		self.initialize()
+	def initialize(self):
+		words = Tkinter.Label(self,text="Please update to continue:",anchor="w")
+		button = Tkinter.Button(self,text="UPDATE",command=self.OnButtonClick)
+		button.grid(column=0,row=1)
+		words.grid(column=0,row=0)
+	def OnButtonClick(self):
+		os.system("xterm -e \"sudo apt-get install wget; sudo wget https://raw.githubusercontent.com/DomThePorcupine/triage/master/triage.py -O /usr/local/bin/triage.py; sudo wget https://raw.githubusercontent.com/DomThePorcupine/triage/master/guifrontend.py -O /usr/local/bin/guifrontend.py; read x\"")
 		self.destroy()
 		
-			
 		
 		
+
+if __name__ == "__main__":
+	update = updater(None)
+	update.title("Triage updater")
+	update.mainloop()
 	
-if __name__ == "__main__":	
 	app = mainGUI(None)
 	app.title("Triage Laucncher")
 	app.mainloop()
